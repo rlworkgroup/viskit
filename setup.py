b10ef9b4ff1a1b58a1d0a9f7f49ab2b989f9e61e
Ryan Julian <ryanjulian@users.noreply.github.com>
2019-07-08 14:25:59 -0700

    Use pytest-xdist for parallel tests

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ extras['dev'] = [
     'pylint',
     'pytest>=3.6',  # Required for pytest-cov on Python 3.6
     'pytest-cov',
+    'pytest-xdist',
     'recommonmark',
     'sphinx',
     'yapf',
